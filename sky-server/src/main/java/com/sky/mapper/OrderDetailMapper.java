49e12a68d16dd7809072ea5f3f402d7acf6ac3b4
zhenzhenqin <3378405015@qq.com>
2025-09-25 22:01:20 +0800

    完成了提交订单相关功能

---
+++ sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
@@ -0,0 +1,18 @@
+package com.sky.mapper;
+
+
+import com.sky.entity.OrderDetail;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface OrderDetailMapper {
+    /**
+     * 批量新增订单详情
+     * @param orderDetailList
+     */
+    void insertBatch(List<OrderDetail> orderDetailList);
+}
+
+
